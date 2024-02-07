4ae4d8e817311f22941eb03f74554abed483ff20
GamageThanuja <w1871525@my.westminster.ac.uk>
2024-02-07 11:28:45 +0530

    create the project background
    create all the screens and add css to navigation screen to make it better

---
+++ SeeQuRe/Screens/urlHistory.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const urlHistory = () => {
+    return(
+        <View>
+            <Text> urlHistory </Text>
+        </View>
+    )
+}
+
+export default urlHistory
+
+const styles = StyleSheet.create({})
