de56c080a2404b51207849438ff4e402fd251e33
Tharuka Gamage <tharu20010621@gmail.com>
2024-02-12 16:17:57 +0530

    Update FeedbackPage.js
    Created the scrolling effect and keyboard dismiss effect

--- SeeQuRe/Screens/Profile/FeedbackPage.js
+++ SeeQuRe/Screens/Profile/FeedbackPage.js
@@ -1,84 +1,115 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { RadioButton } from "react-native-paper";
 
 const FeedbackPage = () => {
   const [checked, setChecked] = useState(false);
   const [text, onChangeText] = useState("");
+  const scrollViewRef = useRef();
 
-  const submitFeedback = () => {
-    console.log("Feedback Submitted:", { rating: checked, comments: text });
+  // const submitFeedback = () => {
+  //   console.log("Feedback Submitted:", { rating: checked, comments: text });
+  // };
+
+  const handleOutsidePress = () => {
+    Keyboard.dismiss();
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>Feedback</Text>
-      </View>
-      <View style={styles.questionContainer}>
-        <Text style={styles.questionText}>
-          How did you feel about the service?
-        </Text>
-        <View style={styles.radioContainer}>
-          <RadioButton
-            value="opt1"
-            status={checked === "opt1" ? "checked" : "unchecked"}
-            onPress={() => setChecked("opt1")}
-            color="#6FCF97"
-          />
-          <Text style={styles.radioText}> Excellent </Text>
-        </View>
-        <View style={styles.radioContainer}>
-          <RadioButton
-            value="opt2"
-            status={checked === "opt2" ? "checked" : "unchecked"}
-            onPress={() => setChecked("opt2")}
-            color="#FFD700"
-          />
-          <Text style={styles.radioText}> Good </Text>
-        </View>
-        <View style={styles.radioContainer}>
-          <RadioButton
-            value="opt3"
-            status={checked === "opt3" ? "checked" : "unchecked"}
-            onPress={() => setChecked("opt3")}
-            color="#FF9F6F"
-          />
-          <Text style={styles.radioText}> Average </Text>
-        </View>
-      </View>
-      <View style={styles.commentSection}>
-        <Text style={styles.questionText}>Additional Comments</Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            multiline
-            placeholder="Share your feedback, comments, and suggestions here..."
-            onChangeText={(newText) => onChangeText(newText)}
-            value={text}
-          />
-          <Text style={{ fontSize: 12, fontWeight: "bold" }}>
-            {" "}
-            ٠ Characters 1000
-          </Text>
-        </View>
-      </View>
-      <TouchableOpacity style={styles.submitButton} onPress={submitFeedback}>
-        <Text style={styles.submitButtonText}>Submit Feedback</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOutsidePress}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "android" ? "padding" : "height"}
+      >
+        <ScrollView
+          ref={scrollViewRef}
+          contentContainerStyle={styles.scrollContainer}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>Feedback</Text>
+          </View>
+          <View style={styles.questionContainer}>
+            <Text style={styles.questionText}>
+              How did you feel about the service?
+            </Text>
+            <View style={styles.radioContainer}>
+              <RadioButton
+                value="opt1"
+                status={checked === "opt1" ? "checked" : "unchecked"}
+                onPress={() => setChecked("opt1")}
+                color="#6FCF97"
+              />
+              <Text style={styles.radioText}> Excellent </Text>
+            </View>
+            <View style={styles.radioContainer}>
+              <RadioButton
+                value="opt2"
+                status={checked === "opt2" ? "checked" : "unchecked"}
+                onPress={() => setChecked("opt2")}
+                color="#FFD700"
+              />
+              <Text style={styles.radioText}> Good </Text>
+            </View>
+            <View style={styles.radioContainer}>
+              <RadioButton
+                value="opt3"
+                status={checked === "opt3" ? "checked" : "unchecked"}
+                onPress={() => setChecked("opt3")}
+                color="#FF9F6F"
+              />
+              <Text style={styles.radioText}> Average </Text>
+            </View>
+          </View>
+          <View style={styles.commentSection}>
+            <Text style={styles.questionText}>Additional Comments</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                multiline
+                placeholder="Share your feedback, comments, and suggestions here..."
+                onChangeText={(newText) => onChangeText(newText)}
+                value={text}
+                onFocus={() =>
+                  scrollViewRef.current.scrollTo({ y: 200, animated: true })
+                }
+              />
+              <Text style={{ fontSize: 12, fontWeight: "bold" }}>
+                {" "}
+                ٠ Characters 1000
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.submitButton}
+            // onPress={submitFeedback}
+          >
+            <Text style={styles.submitButtonText}>Submit Feedback</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default FeedbackPage;
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+
   container: {
     flex: 1,
     padding: 20,
