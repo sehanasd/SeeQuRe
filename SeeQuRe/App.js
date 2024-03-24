8f32a1ad324898c540f0a50abbc0657407dfe588
Shanukahim <shanuka.20212182@iit.ac.lk>
2024-03-24 16:01:16 +0530

    Added landing page

--- SeeQuRe/App.js
+++ SeeQuRe/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,6 +11,7 @@ import Scanner from './Screens/Scanner/ScannerPage';
 import RegisterPage from './Screens/Register/RegisterPage';
 import { ProfilePage } from './Screens/Profile/ProfilePage';
 import FeedbackPage from './Screens/Profile/FeedbackPage';
+import SeeQuRe from './Screens/Landing/Landing';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -19,16 +21,29 @@ const ProfileStack = () => (
     <Stack.Screen name="profile page" component={ProfilePage} />
     <Stack.Screen name="changePassword" component={ChangePassword} />
     <Stack.Screen name="feedback" component={FeedbackPage} />
+   
   </Stack.Navigator>
 );
 
+
 const App = () => {
+  const [isFirstTime, setIsFirstTime] = useState(true);
+
+  useEffect(() => {
+    // Check if it's the first time opening the app
+    // For example, you can use AsyncStorage to store a flag
+    // indicating whether the app has been opened before
+    setIsFirstTime(false);
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName={isFirstTime ? "Landing" : "Login"} screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="landing" component={SeeQuRe} />
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="register" component={RegisterPage} />
         <Stack.Screen name="main" component={MainTabNavigator} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -58,5 +73,13 @@ const MainTabNavigator = () => (
     <Tab.Screen name="profile" component={ProfileStack}  options={{headerShown:false}} />
   </Tab.Navigator>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
